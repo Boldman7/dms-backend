a526a93a411349a7e5f04f7c7d362e5614b7a68c
jmit-modern <jmit.agency@gmail.com>
2025-08-13 03:47:21 -0700

    fix(migration): changed docker-compose.yml to use migration

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -2,3 +2,17 @@ from .post import Post
 from .rate_limit import RateLimit
 from .tier import Tier
 from .user import User
+
+from .collect.company import Company
+from .collect.group import Group
+from .collect.plc_brand import PlcBrand
+from .collect.plc_type import PlcType
+from .collect.smart_hardware_type import SmartHardwareType
+from .collect.smart_hardware import SmartHardware
+from .collect.template import Template
+from .collect.variable import Variable
+
+from .equipment.data_source import DataSource
+from .equipment.device import Device
+from .equipment.product_group import ProductGroup
+from .equipment.product import Product
